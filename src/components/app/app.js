74781cb4151175a458b8b3ee1a090534106b91c4
Yuliia Yevhenieva <yliaaa3@gmail.com>
2020-11-25 18:40:34 +0200

    small change

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,5 +5,5 @@ import './app.css';
 import Input from '../input/input.js';
 
 export default function App(){
-    return <div data-testid="element-app" className="wrapper-app"><Input /></div>
+    return <div className="wrapper-app"><Input /></div>
 }
